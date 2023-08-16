aedd2f664bce53c6010b7f6135ed7384bd1a321c
Eberton <ebertonsouza@unigranrio.br>
2023-08-16 12:48:58 -0300

    feat: create input to search planet

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,31 @@ import TablePlanets from './components/tablePlanets/indexs';
 import usePlanets from './hooks/usePlanets';
 
 function App() {
-  const { allPlanets } = usePlanets();
+  const { allPlanets, searchPlanet, setSearchPlanet, filteredPlanet } = usePlanets();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchPlanet(event.target.value);
+  };
+
   return (
-    <TablePlanets planets={ allPlanets } />
+    <>
+      <label htmlFor="searchPlanet">
+        <span>Search planet:</span>
+      </label>
+      <input
+        type="text"
+        id="searchPlanet"
+        onChange={ handleChange }
+        value={ searchPlanet }
+        placeholder="Planet name"
+        data-testid="name-filter"
+      />
+      {
+        searchPlanet.length > 0
+          ? <TablePlanets planets={ filteredPlanet } />
+          : <TablePlanets planets={ allPlanets } />
+      }
+    </>
   );
 }
 
