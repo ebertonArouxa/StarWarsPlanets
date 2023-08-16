29ea2b0be71c54fcfe9992b0bfdf9ff89263859c
Eberton <ebertonsouza@unigranrio.br>
2023-08-16 15:33:49 -0300

    feat: add how to filter planets with form

--- src/hooks/usePlanets.tsx
+++ src/hooks/usePlanets.tsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from 'react';
-import { Result } from '../type';
+import { Result, FormType } from '../type';
+
+const INITIAL_STATE = {
+  colum: 'population',
+  comparison: 'maior que',
+  number: 0,
+};
 
 export default function usePlanets() {
   const [allPlanets, setAllPlanets] = useState<Result[]>([]);
   const [searchPlanet, setSearchPlanet] = useState('');
+  const [formResult, setFormResult] = useState(INITIAL_STATE);
 
   useEffect(() => {
     const fetchPlanets = async () => {
@@ -16,10 +23,37 @@ export default function usePlanets() {
       setAllPlanets(planetsWithoutResidents);
     };
     fetchPlanets();
-  }, []);
+  }, [searchPlanet]);
 
   const filteredPlanet = allPlanets
     .filter((planet) => planet.name.toLowerCase().includes(searchPlanet));
 
-  return { allPlanets, searchPlanet, setSearchPlanet, filteredPlanet };
+  const formFilter = (form: FormType) => {
+    if (form.comparison === 'maior que') {
+      const planetFiltered = allPlanets
+        .filter((planet: any) => Number(planet[form.colum]) > Number(form.number));
+      return planetFiltered;
+    }
+    if (form.comparison === 'menor que') {
+      const planetFiltered = allPlanets
+        .filter((planet: any) => Number(planet[form.colum]) < Number(form.number));
+      return planetFiltered;
+    }
+    if (form.comparison === 'igual a') {
+      const planetFiltered = allPlanets
+        .filter((planet: any) => Number(planet[form.colum]) === Number(form.number));
+      return planetFiltered;
+    }
+  };
+
+  return {
+    allPlanets,
+    searchPlanet,
+    setSearchPlanet,
+    filteredPlanet,
+    formResult,
+    setFormResult,
+    formFilter,
+    setAllPlanets,
+  };
 }
