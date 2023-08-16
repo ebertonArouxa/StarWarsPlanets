3f7f8c121738c0f4f12197dc59f7d5309e8cdbd8
Eberton <ebertonsouza@unigranrio.br>
2023-08-16 11:39:02 -0300

    refactor: fix how to render app

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import PlanetProvider from './context/planet-provider';
 
 ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+  .createRoot(document.getElementById('root') as HTMLElement)
+  .render(
+    <PlanetProvider>
+      <App />
+    </PlanetProvider>,
+  );
